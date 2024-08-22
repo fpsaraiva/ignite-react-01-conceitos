47d422921d21560962eeabc43c180ce5d283adc5
Fernando Saraiva <fpsaraiva@gmail.com>
2024-08-22 20:26:02 -0300

    created header component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Header } from './components/Header'
 import { Input } from './components/Input'
 import { Button } from './components/Button'
 import { Empty } from './components/List/Empty'
+import { Header as ListHeader } from './components/List/Header'
 
 function App() {
   const [inputValue, setInputValue] = useState('')
@@ -28,6 +29,10 @@ function App() {
         </div>
 
         <div className={styles.tasksList}>
+          <ListHeader
+            tasksCounter={0}
+            checkedTasksCounter={0}
+          />
           <Empty />
         </div>
       </section>
