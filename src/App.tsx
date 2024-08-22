c65afe6c682029b69e15d8fcbb69a5b3fe70ec2f
Fernando Saraiva <fpsaraiva@gmail.com>
2024-08-22 13:31:18 -0300

    created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-function App() {
+import { Header } from './components/Header'
 
+function App() {
   return (
-    <>
-      <h1>todo</h1>
-    </>
+    <main>
+      <Header />
+    </main>
   )
 }
 
